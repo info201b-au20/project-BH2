f160a1f19e811e173c36b024e45388e96a908647
patrickliu022 <patrickliu022@gmail.com>
2020-11-06 11:49:35 -0800

    added dataset

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -1,1 +1,6 @@
-Patrick
+library(tidyverse)
+
+temperature <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv")
+
+temp_rows <- nrow(temperature)
+temp_col <- ncol(temperature)
