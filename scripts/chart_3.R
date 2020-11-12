f009636c944f5ca4a8edfb681b10d3748a2ca5de
Evan Siu <evansiu@Evans-MacBook-Pro.local>
2020-11-12 00:05:24 -0800

    rainfall chart

--- scripts/chart_3.R
+++ scripts/chart_3.R
@@ -1,1 +1,6 @@
-Patrick + everyone!!
+# Patrick + everyone!!
+  
+  library(tidyverse)
+  library(ggplot2)
+  rainfall_year_chart <- ggplot(rainfall, aes(x = year, y = rainfall)) + geom_point(size=2, shape=23)
+
