306391b98656f9ff14cd9b840fa04c197c9da4b7
MILLI WIJENAIKE-BOGLE <mwijbo@uw.edu>
2020-11-12 20:22:14 -0800

    Milli's contributionm

--- scripts/summary_information.R
+++ scripts/summary_information.R
@@ -1,8 +1,24 @@
 # Milli
 
+download.packages(tidyr)
+library(tidyr)
+library(readr)
+library(dplyr)
+
+australia_fires<- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv")
+
 # A function that takes in a dataset and returns a list of info about it:
 summary_info <- list()
-summary_info$num_observations <- nrow(my_dataframe)
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+summary_info$num_observations <- nrow(australia_fires)
+summary_info$maxtemperaturedate <- australia_fires %>%
+filter(temperature == max(temperature, na.rm = T)) %>%
+  select(date)
+summary_info$maxtemperature<-australia_fires %>%
+  filter(temperature == max(temperature,na.rm=T))
+summary_info$mean<-australia_fires %>%
+  filter(temperature == mean(temperature, na.rm= T))%>%
+summary_info$median<-australia_fires %>% 
+  filter(temperature == median(temperature, na.rm= T)) %>%
+
+
+  
