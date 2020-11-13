87e6b0e043ed483c3657d0edaa494a3fc2c5f826
patrickliu022 <patrickliu022@gmail.com>
2020-11-12 21:27:52 -0800

    downloaded data

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -1,6 +1,10 @@
 library(tidyverse)
+library(dplyr)
 
 temperature <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv")
 
 temp_rows <- nrow(temperature)
 temp_col <- ncol(temperature)
+
+num_diff_places <- unique(temperature$city_name) %>% 
+  View()
