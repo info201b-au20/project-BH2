e86718d920f0ebe0f7f7a000bb8cd51d887746a8
patrickliu022 <patrickliu022@gmail.com>
2020-11-13 02:18:23 -0800

    checking temp difference between years

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -14,6 +14,7 @@ num_diff_places <- unique(temperatures$city_name)
 perth_avg_temp_by_years<- temperatures %>% 
   mutate(temperatures, year = format(date, "%Y")) %>% 
   filter(city_name == "PERTH") %>% 
+  filter(temp_type == "max") %>% 
   group_by(year) %>% 
   summarise(mean_temp = mean(temperature, na.rm = T))
 
@@ -22,6 +23,7 @@ perth_avg_temp_by_years<- temperatures %>%
 port_avg_temp_by_years<- temperatures %>% 
   mutate(temperatures, year = format(date, "%Y")) %>% 
   filter(city_name == "PORT") %>% 
+  filter(temp_type == "max") %>% 
   group_by(year) %>% 
   summarise(mean_temp = mean(temperature, na.rm = T))
 
@@ -30,6 +32,7 @@ port_avg_temp_by_years<- temperatures %>%
 kent_avg_temp_by_years<- temperatures %>% 
   mutate(temperatures, year = format(date, "%Y")) %>% 
   filter(city_name == "KENT") %>% 
+  filter(temp_type == "max") %>% 
   group_by(year) %>% 
   summarise(mean_temp = mean(temperature, na.rm = T))
 
@@ -38,6 +41,7 @@ kent_avg_temp_by_years<- temperatures %>%
 brisbane_avg_temp_by_years<- temperatures %>% 
   mutate(temperatures, year = format(date, "%Y")) %>% 
   filter(city_name == "BRISBANE") %>% 
+  filter(temp_type == "max") %>% 
   group_by(year) %>% 
   summarise(mean_temp = mean(temperature, na.rm = T))
 
@@ -46,6 +50,7 @@ brisbane_avg_temp_by_years<- temperatures %>%
 sydney_avg_temp_by_years<- temperatures %>% 
   mutate(temperatures, year = format(date, "%Y")) %>% 
   filter(city_name == "SYDNEY") %>% 
+  filter(temp_type == "max") %>% 
   group_by(year) %>% 
   summarise(mean_temp = mean(temperature, na.rm = T))
 
@@ -62,10 +67,12 @@ canberra_avg_temp_by_years<- temperatures %>%
 melbourne_avg_temp_by_years<- temperatures %>% 
   mutate(temperatures, year = format(date, "%Y")) %>% 
   filter(city_name == "MELBOURNE") %>% 
+  filter(temp_type == "max") %>% 
   group_by(year) %>% 
   summarise(mean_temp = mean(temperature, na.rm = T))
 
 # What does distribution of temperatures tell us about global warming?
+
 
 # Are there any amount of fire occurences that correlate with 
 # rising temperatures?
@@ -75,4 +82,6 @@ melbourne_avg_temp_by_years<- temperatures %>%
 # Are there certain locations that are impacted more than others? In other
 # words, were there any drastic changes in the distribiution of temperatures
 # over the course of the recorded dates?
+
+# plot for temperatures across cities within time frame
 
