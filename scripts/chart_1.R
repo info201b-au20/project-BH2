f94a63685e23f3815c3f92784d9c8d0d5c528170
patrickliu022 <patrickliu022@gmail.com>
2020-11-13 00:51:05 -0800

    analyzed avg temp across cities in dataset

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -1,17 +1,19 @@
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
-temperature <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv")
+temperatures <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv")
 
-temp_rows <- nrow(temperature)
-temp_col <- ncol(temperature)
+temp_rows <- nrow(temperatures)
+temp_col <- ncol(temperatures)
 
-num_diff_places <- unique(temperature$city_name)
+num_diff_places <- unique(temperatures$city_name)
 
 # How has temperature patterns altered over the course of decades?
-perth_by_years<- temperature %>% 
-  mutate(temperature, year = format(date, "%Y"))
 
+perth_by_years<- temperatures %>% 
+  mutate(temperatures, year = format(date, "%Y")) %>% 
+  group_by(city_name == "PERTH", year) %>% 
+  summarise(mean_temp = mean(temperature))
 # What does distribution of temperatures tell us about global warming?
 
 # Are there any amount of fire occurences that correlate with 
@@ -22,3 +24,4 @@ perth_by_years<- temperature %>%
 # Are there certain locations that are impacted more than others? In other
 # words, were there any drastic changes in the distribiution of temperatures
 # over the course of the recorded dates?
+
