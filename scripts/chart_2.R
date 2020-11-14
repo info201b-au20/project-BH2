bc9fe21fc92750d6f955cbb55081e0b9c5733aec
Christian Vargas <christian@Christians-MacBook-Air.local>
2020-11-13 16:44:56 -0800

    chart2

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -6,6 +6,13 @@ library(readr)
 # link for data
 
 nasa_fire <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/MODIS_C6_Australia_and_New_Zealand_7d.csv')
+us_fires <- read_csv('https://raw.githubusercontent.com/BuzzFeedNews/2018-07-wildfire-trends/master/data/us_fires/us_fires_1.csv')
+
 
 fire_rows <- nrow(nasa_fire)
 fire_col <- ncol(nasa_fire)
+
+
+
+perth_location <- nasa_fire%>% 
+  mutate(cords = "" 
